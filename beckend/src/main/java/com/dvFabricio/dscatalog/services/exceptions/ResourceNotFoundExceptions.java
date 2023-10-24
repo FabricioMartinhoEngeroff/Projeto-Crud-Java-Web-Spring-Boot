52425946b72e4d6fdf57aef571980a9ef105ffcd
Amanda Fidelis muraro <fa.engeroff1996@gmail.com>
2023-10-24 06:34:57 -0300

    Beckend Project Category e Product

---
+++ beckend/src/main/java/com/dvFabricio/dscatalog/services/exceptions/ResourceNotFoundExceptions.java
@@ -0,0 +1,10 @@
+package com.dvFabricio.dscatalog.services.exceptions;
+
+public class ResourceNotFoundExceptions extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public ResourceNotFoundExceptions(String msg) {
+		super(msg);
+	}
+
+}
